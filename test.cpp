5024bce31379560ced2fca1ee593bbddfde3c9c8
Armand Falabregues <a.falabregues@gmail.com>
2021-03-14 15:48:00 +0100

    fixed trailing and indentations

--- test.cpp
+++ test.cpp
@@ -14,63 +14,60 @@ int main(int argc, char* argv[])
   }
   Mat image;
   image = imread(argv[1]); // IMREAD_COLOR  test de l'image
-  
+
   Mat gray;
   cvtColor(image, gray, COLOR_BGR2GRAY);
-    
+
   Mat edges;
   Canny(gray,edges,100,200,3);
-    
+
   Mat res;
   cvtColor(edges, res, COLOR_GRAY2BGR);
-    
+
   Mat resP;
   resP = res.clone();
-    
-    
+
+
   vector<Vec4i> linesP;
   HoughLinesP(edges,linesP,1,CV_PI/180, 50,50,10);
-    
-      for( size_t i = 0; i < linesP.size(); i++ )
+
+  for( size_t i = 0; i < linesP.size(); i++ )
   {
-      Vec4i l = linesP[i];
-      line( resP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
-   }
-    
-    
-    imshow("Detected Lines (in red) - Probabilistic Line Transform", resP);
-  
-  
-  
-  
+    Vec4i l = linesP[i];
+    line( resP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, LINE_AA);
+  }
+
+  imshow("Detected Lines (in red) - Probabilistic Line Transform", resP);
+
+
   /*Mat dst;
-  Mat thr;
-  Mat result;      // création des différentes images dont on va avoir besoin
-  Mat new_;
-  Mat edges;
-  Mat res;
-  imshow("image",image);   // affichage de la premiere image
+    Mat thr;
+    Mat result;      // création des différentes images dont on va avoir besoin
+    Mat new_;
+    Mat edges;
+    Mat res;
+    imshow("image",image);   // affichage de la premiere image
 
-  cvtColor(image, new_ , COLOR_BGR2HSV,0);// conversion de l'espace colorimétrique en HSV
-  imshow("new",new_);
+    cvtColor(image, new_ , COLOR_BGR2HSV,0);// conversion de l'espace colorimétrique en HSV
+    imshow("new",new_);
 
-  cvtColor(image,dst, COLOR_BGR2GRAY,0); // image en niveau de gris
-  imshow("gray", dst);
+    cvtColor(image,dst, COLOR_BGR2GRAY,0); // image en niveau de gris
+    imshow("gray", dst);
 
-  inRange(new_, Scalar(0,0,200), Scalar(120,120,255),thr);  // filtrer la couleur blanche dans l'image HSV et stocker l'image résultat dans thr
+    inRange(new_, Scalar(0,0,200), Scalar(120,120,255),thr);  // filtrer la couleur blanche dans l'image HSV et stocker l'image résultat dans thr
 
 
-  imshow("testtt", thr);   // affichage de l'image avec la couleur que l'on cherchait ( image bianire avec couleur que l'on recherche en blanc)
+    imshow("testtt", thr);   // affichage de l'image avec la couleur que l'on cherchait ( image bianire avec couleur que l'on recherche en blanc)
 
-  Canny(thr,edges, 75, 175);  // a partir d'ici c est trouvé sur le net , détection de contour avec transformation de Hough, j ia juste testé, pas regarder comment sa fonctionne encore
-  imshow("edges", edges);
+    Canny(thr,edges, 75, 175);  // a partir d'ici c est trouvé sur le net , détection de contour avec transformation de Hough, j ia juste testé, pas regarder comment sa fonctionne encore
+    imshow("edges", edges);
 
-  cvtColor(edges,res, COLOR_GRAY2RGB);
-  vector<Vec2f> lines;
-  HoughLines(edges,lines,1, CV_PI/180, 150,0,0,0,CV_PI);   // transformée de Hough pour détection de ligne dans l'image
+    cvtColor(edges,res, COLOR_GRAY2RGB);
+    vector<Vec2f> lines;
+    HoughLines(edges,lines,1, CV_PI/180, 150,0,0,0,CV_PI);   // transformée de Hough pour détection de ligne dans l'image
 
-  for(size_t i=0; i<lines.size();i++)
-  {
+    for(size_t i=0; i<lines.size();i++)
+    {
     float rho = lines[i][0], theta = lines[i][1];
     Point pt1, pt2;
     double a = cos(theta), b = sin(theta);
@@ -80,9 +77,9 @@ int main(int argc, char* argv[])
     pt2.x = cvRound(x0 - 1000*(-b));
     pt2.y = cvRound(y0 - 1000*(a));
     line( res, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
-  }
+    }
 
-  imshow(" res", res);*/
+    imshow(" res", res);*/
   waitKey(0);  // programme tourne tant que les images ne sont pas fermées
 
   return 0;
